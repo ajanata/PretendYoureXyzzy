90b3129f66f8157ed288fc7c1de9f4eac74232e9
Andy Janata <ajanata@gmail.com>
2012-01-17 20:36:34 -0800

    cah.bind to bind functions to objects

--- WebContent/js/cah.js
+++ WebContent/js/cah.js
@@ -2,3 +2,19 @@ var cah = {};
 
 cah.DEBUG = true;
 cah.nickname = "";
+
+/**
+ * Binds a function to a "this object". Result is a new function that will do the right thing across
+ * contexts.
+ * 
+ * @param {Object}
+ *          selfObj Object for "this" when calling the returned function.
+ * @param {Function}
+ *          func Function to call with selfObjc as "this".
+ * @returns {Function}
+ */
+cah.bind = function(selfObj, func) {
+  return function() {
+    func.apply(selfObj, arguments);
+  };
+};
