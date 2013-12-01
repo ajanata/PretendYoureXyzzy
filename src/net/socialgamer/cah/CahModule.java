ajanata/PretendYoureXyzzy

commit 638fac780a0b88f74ffcb826e9b869f26baaec50

Remove several game list refresh broadcasts: - never broadcast when a player joins a game (unless it's a new game)  - removed a duplicate broadcast in this situation as well - never broadcast when a player leaves a game (unless the game no longer exists) - never broadcast when a game's options change (unless the state of it having or having not a password changes) - DO broadcast when a game resets to the lobby state (we were broadcasting when a game started but not when it ended)
In an effort to reduce deadlocks, do not actually hold a lock over the list of players in a game and instead make a copy into an array for looping, in several locations.

src/net/socialgamer/cah/data/Game.java
@@ -195,7 +195,8 @@ public class Game {
     data.put(LongPollResponse.NICKNAME, user.getNickname());
     broadcastToPlayers(MessageType.GAME_PLAYER_EVENT, data);
 
-    gameManager.broadcastGameListRefresh();
+    // Don't do this anymore, it was driving up a crazy amount of traffic.
+    // gameManager.broadcastGameListRefresh();
   }
 
   /**
@@ -263,7 +264,8 @@ public class Game {
       data.put(LongPollResponse.NICKNAME, user.getNickname());
       broadcastToPlayers(MessageType.GAME_PLAYER_EVENT, data);
 
-      gameManager.broadcastGameListRefresh();
+      // Don't do this anymore, it was driving up a crazy amount of traffic.
+      // gameManager.broadcastGameListRefresh();
 
       if (host == player) {
         if (players.size() > 0) {
@@ -428,13 +430,13 @@ public class Game {
       info.put(GameInfo.PASSWORD, password);
     }
     info.put(GameInfo.HAS_PASSWORD, password != null && !password.equals(""));
-    synchronized (players) {
-      final List<String> playerNames = new ArrayList<String>(players.size());
-      for (final Player player : players) {
-        playerNames.add(player.toString());
-      }
-      info.put(GameInfo.PLAYERS, playerNames);
+
+    final Player[] playersCopy = players.toArray(new Player[players.size()]);
+    final List<String> playerNames = new ArrayList<String>(playersCopy.length);
+    for (final Player player : playersCopy) {
+      playerNames.add(player.toString());
     }
+    info.put(GameInfo.PLAYERS, playerNames);
     return info;
   }
 
@@ -444,12 +446,11 @@ public class Game {
    */
   public List<Map<GamePlayerInfo, Object>> getAllPlayerInfo() {
     final List<Map<GamePlayerInfo, Object>> info;
-    synchronized (players) {
-      info = new ArrayList<Map<GamePlayerInfo, Object>>(players.size());
-      for (final Player player : players) {
-        final Map<GamePlayerInfo, Object> playerInfo = getPlayerInfo(player);
-        info.add(playerInfo);
-      }
+    final Player[] playersCopy = players.toArray(new Player[players.size()]);
+    info = new ArrayList<Map<GamePlayerInfo, Object>>(playersCopy.length);
+    for (final Player player : playersCopy) {
+      final Map<GamePlayerInfo, Object> playerInfo = getPlayerInfo(player);
+      info.add(playerInfo);
     }
     return info;
   }
@@ -549,14 +550,13 @@ public class Game {
       return false;
     }
     boolean started;
-    synchronized (players) {
-      if (players.size() >= 3) {
-        // Pick a random start judge, though the "next" judge will actually go first.
-        judgeIndex = (int) (Math.random() * players.size());
-        started = true;
-      } else {
-        started = false;
-      }
+    final int numPlayers = players.size();
+    if (numPlayers >= 3) {
+      // Pick a random start judge, though the "next" judge will actually go first.
+      judgeIndex = (int) (Math.random() * numPlayers);
+      started = true;
+    } else {
+      started = false;
     }
     if (started) {
       logger.info(String.format("Starting game %d.", id));
@@ -587,21 +587,19 @@ public class Game {
    * Move the game into the {@code DEALING} state, and deal cards. The game immediately then moves
    * into the {@code PLAYING} state.
    * <br/>
-   * Synchronizes on {@link #players}.
    */
   private void dealState() {
     state = GameState.DEALING;
-    synchronized (players) {
-      for (final Player player : players) {
-        final List<WhiteCard> hand = player.getHand();
-        final List<WhiteCard> newCards = new LinkedList<WhiteCard>();
-        while (hand.size() < 10) {
-          final WhiteCard card = getNextWhiteCard();
-          hand.add(card);
-          newCards.add(card);
-        }
-        sendCardsToPlayer(player, newCards);
+    final Player[] playersCopy = players.toArray(new Player[players.size()]);
+    for (final Player player : playersCopy) {
+      final List<WhiteCard> hand = player.getHand();
+      final List<WhiteCard> newCards = new LinkedList<WhiteCard>();
+      while (hand.size() < 10) {
+        final WhiteCard card = getNextWhiteCard();
+        hand.add(card);
+        newCards.add(card);
       }
+      sendCardsToPlayer(player, newCards);
     }
     playingState();
   }
@@ -761,15 +759,14 @@ public class Game {
           logger.info(String.format("Skipping idle player %s in game %d.",
               player.getUser().toString(), id));
           player.skipped();
-          final HashMap<ReturnableData, Object> data = getEventMap();
 
+          final HashMap<ReturnableData, Object> data = getEventMap();
+          data.put(LongPollResponse.NICKNAME, player.getUser().getNickname());
           if (player.getSkipCount() >= MAX_SKIPS_BEFORE_KICK || playedCards.size() < 2) {
             data.put(LongPollResponse.EVENT, LongPollEvent.GAME_PLAYER_KICKED_IDLE.toString());
-            data.put(LongPollResponse.NICKNAME, player.getUser().getNickname());
             playersToRemove.add(player.getUser());
           } else {
             data.put(LongPollResponse.EVENT, LongPollEvent.GAME_PLAYER_SKIPPED.toString());
-            data.put(LongPollResponse.NICKNAME, player.getUser().getNickname());
             playersToUpdateStatus.add(player);
           }
           broadcastToPlayers(MessageType.GAME_EVENT, data);
@@ -913,6 +910,8 @@ public class Game {
       data.put(LongPollResponse.PLAYER_INFO, getPlayerInfo(judge));
       broadcastToPlayers(MessageType.GAME_PLAYER_EVENT, data);
     }
+
+    gameManager.broadcastGameListRefresh();
   }
 
   /**
@@ -1015,19 +1014,16 @@ public class Game {
   /**
    * Get the {@code Player} object for a given {@code User} object.
    * 
-   * Synchronizes on {@link #players}.
-   * 
    * @param user
    * @return The {@code Player} object representing {@code user} in this game, or {@code null} if
    *         {@code user} is not in this game.
    */
   @Nullable
   private Player getPlayerForUser(final User user) {
-    synchronized (players) {
-      for (final Player player : players) {
-        if (player.getUser() == user) {
-          return player;
-        }
+    final Player[] playersCopy = players.toArray(new Player[players.size()]);
+    for (final Player player : playersCopy) {
+      if (player.getUser() == user) {
+        return player;
       }
     }
     return null;
@@ -1159,11 +1155,10 @@ public class Game {
    */
   private List<User> playersToUsers() {
     final List<User> users;
-    synchronized (players) {
-      users = new ArrayList<User>(players.size());
-      for (final Player player : players) {
-        users.add(player.getUser());
-      }
+    final Player[] playersCopy = players.toArray(new Player[players.size()]);
+    users = new ArrayList<User>(playersCopy.length);
+    for (final Player player : playersCopy) {
+      users.add(player.getUser());
     }
     return users;
   }

WebContent/game.jsp
@@ -95,12 +95,14 @@ HttpSession hSession = request.getSession(true);
     If this is your first time playing, you may wish to read <a href="/">the changelog and list of
     known issues</a>.
   </p>
-  <p tabindex="0">Most recent update: 28 April 2013:</p>
+  <p tabindex="0">Most recent update: 7 August 2013:</p>
   <ul>
-    <li tabindex="0">Version 1.3 of the base Cards Against Humanity game.</li>
-    <li tabindex="0">Card sets are grouped by official or custom.</li>
-    <li tabindex="0">You can <a href="viewcards.jsp">view all of the cards in the game</a>,
-    including searching by text and filtering card set.</li>
+    <li tabindex="0"><strong>The game list will not automatically update all the time now.</strong>
+    You will need to start using the Refresh Games button. The game list will automatically update
+    for new games, removed games, when games become passworded, or when you leave a game.</li>
+    <li tabindex="0">A lot of custom card sets have been added.</li>
+    <li tabindex="0">Cleaned up some error handling.</li>
+    <li tabindex="0">Tried to fix some more of the server crashes.</li>
   </ul>
   <div id="nickbox">
     Nickname:

WebContent/index.jsp
@@ -54,6 +54,14 @@ to, for instance, display the number of connected players.
 </p>
 <p>Recent Changes:</p>
 <ul>
+  <li>7 August 2013:<ul>
+    <li tabindex="0"><strong>The game list will not automatically update all the time now.</strong>
+    You will need to start using the Refresh Games button. The game list will automatically update
+    for new games, removed games, when games become passworded, or when you leave a game.</li>
+    <li tabindex="0">A lot of custom card sets have been added.</li>
+    <li tabindex="0">Cleaned up some error handling.</li>
+    <li tabindex="0">Tried to fix some more of the server crashes.</li>
+  </ul></li>
   <li>28 April 2013:<ul>
     <li tabindex="0">Version 1.3 of the base Cards Against Humanity game.</li>
     <li tabindex="0">Card sets are grouped by official or custom.</li>

src/net/socialgamer/cah/handlers/ChangeGameOptionHandler.java
@@ -51,6 +51,7 @@ public class ChangeGameOptionHandler extends GameWithPlayerHandler {
                 Integer.parseInt(cardSetId)));
           }
         }
+        final String oldPassword = game.getPassword();
         String password = request.getParameter(AjaxRequest.PASSWORD);
         if (password == null) {
           password = "";
@@ -63,11 +64,16 @@ public class ChangeGameOptionHandler extends GameWithPlayerHandler {
           useTimer = Boolean.valueOf(useTimerString);
         }
         game.updateGameSettings(scoreLimit, playerLimit, cardSets, password, useTimer);
+
+        // only broadcast an update if the password state has changed, because it needs to change
+        // the text on the join button and the sort order
+        if (!password.equals(oldPassword)) {
+          gameManager.broadcastGameListRefresh();
+        }
       } catch (final NumberFormatException nfe) {
         return error(ErrorCode.BAD_REQUEST);
       }
 
-      gameManager.broadcastGameListRefresh();
       return data;
     }
   }

src/net/socialgamer/cah/handlers/JoinGameHandler.java
@@ -74,7 +74,6 @@ public class JoinGameHandler extends GameHandler {
     } catch (final TooManyPlayersException e) {
       return error(ErrorCode.GAME_FULL);
     }
-    gameManager.broadcastGameListRefresh();
     return data;
   }
 }

commit 8221ab7b54f9f8ade34ff8c06d4213c9fa6090a6

- Remove some pointless @Provides methods and .bind().toInstance(). - Use a single global Timer and schedule multiple TimerTasks on it. Moving to a ScheduledThreadPoolExecutor may be preferable as now all TimerTasks are serialized. - Fix tests that were broken by changed behavior in 638fac780a0b88f74ffcb826e9b869f26baaec50 (shows how often I run the few crappy tests I have...)

src/net/socialgamer/cah/CahModule.java
@@ -29,6 +29,7 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Properties;
 import java.util.Set;
+import java.util.Timer;
 
 import net.socialgamer.cah.data.GameManager;
 import net.socialgamer.cah.data.GameManager.GameId;
@@ -39,6 +40,7 @@ import org.hibernate.Session;
 import com.google.inject.AbstractModule;
 import com.google.inject.BindingAnnotation;
 import com.google.inject.Provides;
+import com.google.inject.TypeLiteral;
 
 
 /**
@@ -48,9 +50,25 @@ import com.google.inject.Provides;
  */
 public class CahModule extends AbstractModule {
 
+  private final static Properties properties = new Properties();
+
   @Override
   protected void configure() {
-    bind(Integer.class).annotatedWith(GameId.class).toProvider(GameManager.class);
+    bind(Integer.class)
+        .annotatedWith(GameId.class)
+        .toProvider(GameManager.class);
+    /*
+     * A mutable Set of IP addresses (in String format) which are banned. This Set is
+     * thread-safe.
+     */
+    bind(new TypeLiteral<Set<String>>() {
+    })
+        .annotatedWith(BanList.class)
+        .toInstance(Collections.synchronizedSet(new HashSet<String>()));
+    bind(Timer.class)
+        .toInstance(new Timer("timer-task", true));
+    bind(Properties.class)
+        .toInstance(properties);
   }
 
   /**
@@ -71,11 +89,6 @@ public class CahModule extends AbstractModule {
     return Integer.valueOf((String) properties.get("pyx.server.max_users"));
   }
 
-  @BindingAnnotation
-  @Retention(RetentionPolicy.RUNTIME)
-  public @interface MaxUsers {
-  }
-
   /**
    * @return A Hibernate session. Objects which receive a Hibernate session should close the
    * session when they are done!
@@ -85,27 +98,13 @@ public class CahModule extends AbstractModule {
     return HibernateUtil.instance.sessionFactory.openSession();
   }
 
-  private final static Set<String> banList = Collections.synchronizedSet(new HashSet<String>());
-
-  /**
-   * @return A mutable Set of IP addresses (in String format) which are banned. This Set is
-   * thread-safe.
-   */
-  @Provides
-  @BanList
-  Set<String> provideBanList() {
-    return banList;
-  }
-
   @BindingAnnotation
   @Retention(RetentionPolicy.RUNTIME)
   public @interface BanList {
   }
 
-  private final static Properties properties = new Properties();
-
-  @Provides
-  Properties provideProperties() {
-    return properties;
+  @BindingAnnotation
+  @Retention(RetentionPolicy.RUNTIME)
+  public @interface MaxUsers {
   }
 }

src/net/socialgamer/cah/StartupUtils.java
@@ -64,11 +64,6 @@ public class StartupUtils extends GuiceServletContextListener {
   private static final long PING_CHECK_DELAY = 5 * 1000;
 
   /**
-   * Context attribute key name for the disconnected client timer.
-   */
-  private static final String PING_TIMER_NAME = "ping_timer";
-
-  /**
    * Context attribute key name for the time the server was started.
    */
   public static final String DATE_NAME = "started_at";
@@ -86,10 +81,11 @@ public class StartupUtils extends GuiceServletContextListener {
   @Override
   public void contextDestroyed(final ServletContextEvent contextEvent) {
     final ServletContext context = contextEvent.getServletContext();
-    final Timer timer = (Timer) context.getAttribute(PING_TIMER_NAME);
-    assert (timer != null);
+
+    final Injector injector = (Injector) context.getAttribute(INJECTOR);
+    final Timer timer = injector.getInstance(Timer.class);
     timer.cancel();
-    context.removeAttribute(PING_TIMER_NAME);
+
     context.removeAttribute(INJECTOR);
     context.removeAttribute(DATE_NAME);
 
@@ -101,10 +97,9 @@ public class StartupUtils extends GuiceServletContextListener {
     final ServletContext context = contextEvent.getServletContext();
     final Injector injector = getInjector();
     final UserPing ping = injector.getInstance(UserPing.class);
-    final Timer timer = new Timer("PingCheck", true);
+    final Timer timer = injector.getInstance(Timer.class);
     timer.schedule(ping, PING_START_DELAY, PING_CHECK_DELAY);
     serverStarted = new Date();
-    context.setAttribute(PING_TIMER_NAME, timer);
     context.setAttribute(INJECTOR, injector);
     context.setAttribute(DATE_NAME, serverStarted);
 

src/net/socialgamer/cah/data/Game.java
@@ -134,12 +134,18 @@ public class Game {
    * Lock object to prevent judging during idle judge detection and vice-versa.
    */
   private final Object judgeLock = new Object();
-  private Timer nextRoundTimer;
-  private final Object nextRoundTimerLock = new Object();
+
+  /**
+   * Lock to prevent missing timer updates.
+   */
+  private final Object roundTimerLock = new Object();
+  private volatile TimerTask lastTimerTask;
+  private final Timer globalTimer;
+
   private int scoreGoal = 8;
   private final Set<CardSet> cardSets = new HashSet<CardSet>();
   private String password = "";
-  private boolean useTimer = true;
+  private boolean useIdleTimer = true;
   private final Session hibernateSession;
 
   /**
@@ -152,14 +158,17 @@ public class Game {
    * @param gameManager
    *          The game manager, for broadcasting game list refresh notices and destroying this game
    *          when everybody leaves.
+   * @param hibernateSession Hibernate session from which to load cards.
+   * @param globalTimer The global timer on which to schedule tasks.
    */
   @Inject
   public Game(@GameId final Integer id, final ConnectedUsers connectedUsers,
-      final GameManager gameManager, final Session hibernateSession) {
+      final GameManager gameManager, final Session hibernateSession, final Timer globalTimer) {
     this.id = id;
     this.connectedUsers = connectedUsers;
     this.gameManager = gameManager;
     this.hibernateSession = hibernateSession;
+    this.globalTimer = globalTimer;
     state = GameState.LOBBY;
   }
 
@@ -207,7 +216,7 @@ public class Game {
    * Remove a player from the game.
    * <br/>
    * Synchronizes on {@link #players}, {@link #playedCards}, {@link #whiteDeck}, and
-   * {@link #nextRoundTimerLock}.
+   * {@link #roundTimerLock}.
    * 
    * @param user
    *          Player to remove from the game.
@@ -290,15 +299,14 @@ public class Game {
             id));
         resetState(true);
       } else if (wasJudge) {
-        synchronized (nextRoundTimerLock) {
+        synchronized (roundTimerLock) {
           final TimerTask task = new TimerTask() {
             @Override
             public void run() {
               startNextRound();
             }
           };
-          nextRoundTimer = new Timer("judge-left-" + id, true);
-          nextRoundTimer.schedule(task, ROUND_INTERMISSION);
+          rescheduleTimer(task, ROUND_INTERMISSION);
         }
       }
       return players.size() == 0;
@@ -442,7 +450,7 @@ public class Game {
     }
     this.maxBlanks = newMaxBlanks;
     this.password = newPassword;
-    this.useTimer = newUseTimer;
+    this.useIdleTimer = newUseTimer;
 
     final HashMap<ReturnableData, Object> data = getEventMap();
     data.put(LongPollResponse.EVENT, LongPollEvent.GAME_OPTIONS_CHANGED.toString());
@@ -493,7 +501,7 @@ public class Game {
     info.put(GameInfo.PLAYER_LIMIT, maxPlayers);
     info.put(GameInfo.SPECTATOR_LIMIT, maxSpectators);
     info.put(GameInfo.SCORE_LIMIT, scoreGoal);
-    info.put(GameInfo.USE_TIMER, useTimer);
+    info.put(GameInfo.USE_TIMER, useIdleTimer);
     if (includePassword) {
       info.put(GameInfo.PASSWORD, password);
     }
@@ -684,7 +692,7 @@ public class Game {
    * Move the game into the {@code PLAYING} state, drawing a new Black Card and dispatching a
    * message to all players.
    * <br/>
-   * Synchronizes on {@link #players}, {@link #blackCardLock}, and {@link #nextRoundTimerLock}.
+   * Synchronizes on {@link #players}, {@link #blackCardLock}, and {@link #roundTimerLock}.
    */
   private void playingState() {
     state = GameState.PLAYING;
@@ -716,7 +724,7 @@ public class Game {
     }
 
     // Perhaps figure out a better way to do this...
-    final int playTimer = useTimer ? PLAY_TIMEOUT_BASE
+    final int playTimer = useIdleTimer ? PLAY_TIMEOUT_BASE
         + (PLAY_TIMEOUT_PER_CARD * blackCard.getPick()) : Integer.MAX_VALUE;
 
     final HashMap<ReturnableData, Object> data = getEventMap();
@@ -727,8 +735,7 @@ public class Game {
 
     broadcastToPlayers(MessageType.GAME_EVENT, data);
 
-    synchronized (nextRoundTimerLock) {
-      killRoundTimer();
+    synchronized (roundTimerLock) {
       final TimerTask task = new TimerTask() {
         @Override
         public void run() {
@@ -736,19 +743,18 @@ public class Game {
         }
       };
       // 10 second warning
-      nextRoundTimer = new Timer("hurry-up-" + id, true);
-      nextRoundTimer.schedule(task, playTimer - 10 * 1000);
+      rescheduleTimer(task, playTimer - 10 * 1000);
     }
   }
 
   /**
    * Warn players that have not yet played that they are running out of time to do so.
    * <br/>
-   * Synchronizes on {@link #nextRoundTimerLock} and {@link #roundPlayers}.
+   * Synchronizes on {@link #roundTimerLock} and {@link #roundPlayers}.
    */
   private void warnPlayersToPlay() {
     // have to do this all synchronized in case they play while we're processing this
-    synchronized (nextRoundTimerLock) {
+    synchronized (roundTimerLock) {
       killRoundTimer();
 
       synchronized (roundPlayers) {
@@ -771,14 +777,13 @@ public class Game {
         }
       };
       // 10 seconds to finish playing
-      nextRoundTimer = new Timer("hurry-up-" + id, true);
-      nextRoundTimer.schedule(task, 10 * 1000);
+      rescheduleTimer(task, 10 * 1000);
     }
   }
 
   private void warnJudgeToJudge() {
     // have to do this all synchronized in case they play while we're processing this
-    synchronized (nextRoundTimerLock) {
+    synchronized (roundTimerLock) {
       killRoundTimer();
 
       if (state == GameState.JUDGING) {
@@ -796,8 +801,7 @@ public class Game {
         }
       };
       // 10 seconds to finish playing
-      nextRoundTimer = new Timer("hurry-up-" + id, true);
-      nextRoundTimer.schedule(task, 10 * 1000);
+      rescheduleTimer(task, 10 * 1000);
     }
   }
 
@@ -886,11 +890,21 @@ public class Game {
   }
 
   private void killRoundTimer() {
-    synchronized (nextRoundTimerLock) {
-      if (nextRoundTimer != null) {
-        nextRoundTimer.cancel();
-        nextRoundTimer = null;
+    synchronized (roundTimerLock) {
+      if (lastTimerTask != null) {
+        logger.trace(String.format("Killing timer task %s", lastTimerTask));
+        lastTimerTask.cancel();
+        lastTimerTask = null;
       }
+    }
+  }
+
+  private void rescheduleTimer(final TimerTask task, final long timeout) {
+    synchronized (roundTimerLock) {
+      killRoundTimer();
+      logger.trace(String.format("Scheduling timer task %s after %d ms", task, timeout));
+      lastTimerTask = task;
+      globalTimer.schedule(task, timeout);
     }
   }
 
@@ -902,7 +916,7 @@ public class Game {
     state = GameState.JUDGING;
 
     // Perhaps figure out a better way to do this...
-    final int judgeTimer = useTimer ? JUDGE_TIMEOUT_BASE
+    final int judgeTimer = useIdleTimer ? JUDGE_TIMEOUT_BASE
         + (JUDGE_TIMEOUT_PER_CARD * playedCards.size() * blackCard.getPick()) : Integer.MAX_VALUE;
 
     final HashMap<ReturnableData, Object> data = getEventMap();
@@ -914,8 +928,7 @@ public class Game {
 
     notifyPlayerInfoChange(getJudge());
 
-    synchronized (nextRoundTimerLock) {
-      killRoundTimer();
+    synchronized (roundTimerLock) {
       final TimerTask task = new TimerTask() {
         @Override
         public void run() {
@@ -923,8 +936,7 @@ public class Game {
         }
       };
       // 10 second warning
-      nextRoundTimer = new Timer("hurry-up-" + id, true);
-      nextRoundTimer.schedule(task, judgeTimer - 10 * 1000);
+      rescheduleTimer(task, judgeTimer - 10 * 1000);
     }
   }
 
@@ -1007,12 +1019,7 @@ public class Game {
    * state.
    */
   private void startNextRound() {
-    synchronized (nextRoundTimerLock) {
-      if (nextRoundTimer != null) {
-        nextRoundTimer.cancel();
-        nextRoundTimer = null;
-      }
-    }
+    killRoundTimer();
 
     synchronized (playedCards) {
       for (final List<WhiteCard> cards : playedCards.cards()) {
@@ -1358,8 +1365,7 @@ public class Game {
     notifyPlayerInfoChange(getJudge());
     notifyPlayerInfoChange(cardPlayer);
 
-    synchronized (nextRoundTimerLock) {
-      killRoundTimer();
+    synchronized (roundTimerLock) {
       final TimerTask task;
       // TODO win-by-x option
       if (cardPlayer.getScore() >= scoreGoal) {
@@ -1377,8 +1383,7 @@ public class Game {
           }
         };
       }
-      nextRoundTimer = new Timer("round-intermission-" + id, true);
-      nextRoundTimer.schedule(task, ROUND_INTERMISSION);
+      rescheduleTimer(task, ROUND_INTERMISSION);
     }
 
     return null;

test/net/socialgamer/cah/data/GameManagerTest.java
@@ -35,6 +35,7 @@ import static org.junit.Assert.assertNull;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Timer;
 
 import net.socialgamer.cah.HibernateUtil;
 import net.socialgamer.cah.data.GameManager.GameId;
@@ -64,6 +65,7 @@ public class GameManagerTest {
   private ConnectedUsers cuMock;
   private User userMock;
   private int gameId;
+  private final Timer timer = new Timer("junit-timer", true);
 
   @Before
   public void setUp() throws Exception {
@@ -116,11 +118,11 @@ public class GameManagerTest {
 
     // fill it up with 3 games
     assertEquals(0, gameManager.get().intValue());
-    gameManager.getGames().put(0, new Game(0, cuMock, gameManager, null));
+    gameManager.getGames().put(0, new Game(0, cuMock, gameManager, null, timer));
     assertEquals(1, gameManager.get().intValue());
-    gameManager.getGames().put(1, new Game(1, cuMock, gameManager, null));
+    gameManager.getGames().put(1, new Game(1, cuMock, gameManager, null, timer));
     assertEquals(2, gameManager.get().intValue());
-    gameManager.getGames().put(2, new Game(2, cuMock, gameManager, null));
+    gameManager.getGames().put(2, new Game(2, cuMock, gameManager, null, timer));
     // make sure it says it can't make any more
     assertEquals(-1, gameManager.get().intValue());
 
@@ -128,13 +130,13 @@ public class GameManagerTest {
     gameManager.destroyGame(1);
     // make sure it re-uses that id
     assertEquals(1, gameManager.get().intValue());
-    gameManager.getGames().put(1, new Game(1, cuMock, gameManager, null));
+    gameManager.getGames().put(1, new Game(1, cuMock, gameManager, null, timer));
     assertEquals(-1, gameManager.get().intValue());
 
     // remove game 1 out from under it, to make sure it'll fix itself
     gameManager.getGames().remove(1);
     assertEquals(1, gameManager.get().intValue());
-    gameManager.getGames().put(1, new Game(1, cuMock, gameManager, null));
+    gameManager.getGames().put(1, new Game(1, cuMock, gameManager, null, timer));
     assertEquals(-1, gameManager.get().intValue());
 
     gameManager.destroyGame(2);
@@ -146,7 +148,7 @@ public class GameManagerTest {
   @Test
   public void testCreateGame() {
     cuMock.broadcastToAll(eq(MessageType.GAME_EVENT), anyObject(HashMap.class));
-    expectLastCall().times(6);
+    expectLastCall().times(3);
     cuMock.broadcastToList(anyObject(Collection.class), eq(MessageType.GAME_PLAYER_EVENT),
         anyObject(HashMap.class));
     expectLastCall().times(3);

test/net/socialgamer/cah/data/GameTest.java
@@ -36,6 +36,7 @@ import static org.junit.Assert.assertTrue;
 
 import java.util.Collection;
 import java.util.HashMap;
+import java.util.Timer;
 
 import net.socialgamer.cah.data.Game.TooManyPlayersException;
 import net.socialgamer.cah.data.QueuedMessage.MessageType;
@@ -54,12 +55,13 @@ public class GameTest {
   private Game game;
   private ConnectedUsers cuMock;
   private GameManager gmMock;
+  private final Timer timer = new Timer("junit-timer", true);
 
   @Before
   public void setUp() throws Exception {
     cuMock = createMock(ConnectedUsers.class);
     gmMock = createMock(GameManager.class);
-    game = new Game(0, cuMock, gmMock, null);
+    game = new Game(0, cuMock, gmMock, null, timer);
   }
 
   @SuppressWarnings("unchecked")
@@ -67,8 +69,6 @@ public class GameTest {
   public void testRemovePlayer() throws IllegalStateException, TooManyPlayersException {
     cuMock.broadcastToList(anyObject(Collection.class), eq(MessageType.GAME_PLAYER_EVENT),
         anyObject(HashMap.class));
-    expectLastCall().times(4);
-    gmMock.broadcastGameListRefresh();
     expectLastCall().times(4);
     replay(cuMock);
     gmMock.destroyGame(anyInt());