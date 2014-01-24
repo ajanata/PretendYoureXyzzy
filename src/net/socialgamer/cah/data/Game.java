502a2b10b6600ac534a1c78ff3ea78ec75894644
uecasm <github@mirality.co.nz>
2014-01-24 23:35:59 +1300

    Fixes #33.

--- src/net/socialgamer/cah/data/Game.java
+++ src/net/socialgamer/cah/data/Game.java
@@ -1007,6 +1007,9 @@ public class Game {
    * @return True if judging should begin.
    */
   private boolean startJudging() {
+    if (state != GameState.PLAYING) {
+      return false;
+    }
     if (playedCards.size() == roundPlayers.size()) {
       boolean startJudging = true;
       for (final List<WhiteCard> cards : playedCards.cards()) {
