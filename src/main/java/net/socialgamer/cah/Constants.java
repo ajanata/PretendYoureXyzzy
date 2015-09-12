7c2b08c81dca9ef02c2eabcba0d5abf69ddd7d00
Andy Janata <ajanata@gmail.com>
2015-09-12 09:14:34 -0700

    Update admin IP address (really need to make this a config option...)

--- src/main/java/net/socialgamer/cah/Constants.java
+++ src/main/java/net/socialgamer/cah/Constants.java
@@ -54,7 +54,7 @@ public class Constants {
       add("0:0:0:0:0:0:0:1");
       add("127.0.0.1");
       // ajanata
-      add("73.202.162.31");
+      add("73.222.116.50");
       // vsTerminus
       add("207.161.130.75");
     }
